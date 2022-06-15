8df67478a296aa5dbe8d798e6d41885a07dbf9a3
rusilko <tomek.rusilko@lunarlogic.io>
2022-06-15 16:43:05 +0200

    Day 23: Finish GuessTheFlag App

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,26 +8,86 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var showingScore = false
+  @State private var scoreTitle = ""
+  @State private var countries = ["Estonia", "France", "Monaco", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+  @State private var correctAnswer = Int.random(in: 0..<3)
+  @State private var message = ""
+  @State private var score = 0
+  
   var body: some View {
-    VStack {
-    Button("Button 0") { }
-        .tint(.blue)
-    Button("Button 1") { }
-      .buttonStyle(.bordered)
-      .tint(.purple)
-    Button("Button 2", role: .destructive) { }
-      .buttonStyle(.bordered)
-      .tint(.mint)
-    Button("Button 3") { }
-      .buttonStyle(.borderedProminent)
-    Button("Button 4", role: .destructive) { }
-      .buttonStyle(.borderedProminent)
-      .tint(.mint)
-  }
+    ZStack {
+      RadialGradient(stops: [
+        .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+        .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+      ], center: .top, startRadius: 200, endRadius: 400)
+      .ignoresSafeArea()
+      
+      VStack {
+        VStack(spacing: 15) {
+          Spacer()
+          Text("Guess the Flag")
+            .font(.largeTitle.bold())
+            .foregroundColor(.white)
+          
+          VStack {
+            Text("Tap the flag of")
+              .font(.subheadline.weight(.heavy))
+              .foregroundColor(.secondary)
+            Text(countries[correctAnswer])
+              .font(.largeTitle.weight(.semibold))
+          }
+          
+          ForEach(0..<3) { number in
+            Button {
+              flagTapped(number)
+              
+            } label: {
+              Image(countries[number])
+                .renderingMode(.original)
+                .clipShape(Capsule())
+                .shadow(radius: 5)
+            }
+          }
+          
+          Spacer()
+          Spacer()
+          Text("Score: \(score)")
+            .foregroundColor(.white)
+            .font(.title.bold())
+          Spacer()
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.vertical, 20)
+        .background(.regularMaterial)
+        .clipShape(RoundedRectangle(cornerRadius: 20))
+      }
+      .padding(10)
+    }
+    .alert(scoreTitle, isPresented: $showingScore) {
+      Button("Continue", action: askQuestion)
+    } message: {
+      Text(message)
+      Text("Your score is \(score)")
+    }
   }
   
-  func executeDelete() {
-    print("Now deleting…")
+  func flagTapped(_ number: Int) {
+    if number == correctAnswer {
+      scoreTitle = "Correct"
+      message = ""
+      score += 1
+    } else {
+      scoreTitle = "Wrong"
+      message = "That's the flag of \(countries[correctAnswer])"
+    }
+    
+    showingScore = true
+  }
+  
+  func askQuestion() {
+    countries.shuffle()
+    correctAnswer = Int.random(in: 0..<3)
   }
 }
 
--- GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct GuessTheFlagApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
